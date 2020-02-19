e10ad42497976e25601ad6e45a38e575267011f5
ericbanker12 <ericbanker12@gmail.com>
2020-02-19 17:01:21 -0600

    WIP rock_paper_scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,23 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  outputs = ['rock', 'paper', 'scissors']
+  permutations = []
 
+  if n < 1:
+    return [[]]
+
+  for hand in outputs:
+    permutations.append([hand])
+  
+  while len(permutations[-1]) < n:
+    for arr in permutations.copy():
+      for hand in outputs:
+        permutations.append([*arr, hand])
+  
+  return [perm for perm in permutations if len(perm) == n]
+
+# print(rock_paper_scissors(3))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
