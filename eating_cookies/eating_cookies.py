4a4876608e297ec5dbfba04723bf74fd74082c94
ericbanker12 <ericbanker12@gmail.com>
2020-02-19 16:16:06 -0600

    complete eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,28 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  # 1,1,2,4,7,13,24,44,81,149... n=(n-1)+(n-2)+(n-3)
+  if not cache or not cache[0]:
+    cache = [1,1,2]
+  try:
+    return cache[n]
+  except IndexError:
+    result = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    cache.append(result)
+    return result
+
+import random
+def generate_perm(n):
+  """generate permutations through brute force"""
+  results = set()
+  for i in range(1,n+1):
+    for j in range(100000):
+      test = [random.choice([1,2,3]) for k in range(i)]
+      if sum(test) == n:
+        results.add(str(test))
+  return results
+
+# print(len(generate_perm(3)))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
