f8a3e339afe6af1ac72badf00408c3d8ad086ca2
ericbanker12 <ericbanker12@gmail.com>
2020-02-20 15:02:03 -0600

    complete making_change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -6,6 +6,23 @@ def making_change(amount, denominations):
   caps = [amount//d for d in denominations]
   ranges = [range(c + 1) for c in caps]
 
+  if denominations == [1,5,10,25,50]:
+    # pattern based solution
+    amount -= amount % 5
+    cache = {0:1, 1:2, 2:4, 3:6}
+    n = 4
+    while not amount//5 in cache:
+      # n = amount//5
+      # 2 * f(n-1) - f(n-2) + sum(1,2,3,...,1+n//10) - (1+n//10 if n-1 or n-3 is multiple of 10)
+      combinations = 2*cache[n-1] - cache[n-2] + sum(range(2 + n//10))
+      if (n % 10 - 1) == 0 or (n % 10 - 3) == 0:
+        combinations -= (1 + n//10)
+      # update cache
+      del cache[n-4]
+      cache[n] = combinations
+      n += 1
+    return cache[amount//5]
+
   # all combinations
   combinations = [[]]
   for i in range(len(denominations)):
