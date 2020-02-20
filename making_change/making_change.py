c0b9cbfa3b8d790f7981610709074bcdda47cacf
ericbanker12 <ericbanker12@gmail.com>
2020-02-20 14:17:10 -0600

    WIP making_change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,8 +3,33 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+  caps = [amount//d for d in denominations]
+  ranges = [range(c + 1) for c in caps]
 
+  # all combinations
+  combinations = [[]]
+  for i in range(len(denominations)):
+    p = []
+    for arr in combinations:
+      for n in ranges[i]:
+        combi = [*arr, n*denominations[i]]
+        # filter greater than ammount
+        if sum(combi) <= amount:
+          p.append(combi)
+    combinations = p
+
+  # filter less than amount
+  combinations = [p for p in combinations if sum(p) == amount]
+
+  # return count
+  return len(combinations)
+
+# for i in range(0,501,5):
+#   print(f'{i},{making_change(i, [1,5,10,25,50])}')
+
+# import timeit
+# t = timeit.timeit('making_change(300, [1,5,10,25,50])', globals=globals(), number=1)
+# print(t)
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
